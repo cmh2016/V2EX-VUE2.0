751cc15c4d6e56d912c7e47d0ed36699d6aadae2
cmh2016 <15675523332@163.com>
2017-02-27 10:56:32 +0800

    主题页面增加分页、下拉刷新和搜索功能

--- src/view/node.vue
+++ src/view/node.vue
@@ -2,13 +2,13 @@
 <div class="node">
  <x-loading v-show="loading"></x-loading>
 	<x-header></x-header>
-	 <search  v-model.lazy="word" @on-change="getResult" v-show="searchShow" placeholder="搜索主题" cancelText="取消" position="absolute" auto-scroll-to-top top="46px"></search>
+	 <search  @on-result-click="clickResuilt" v-model.lazy="word" :results="results" @on-change="getResult" v-show="searchShow" placeholder="搜索主题" cancelText="取消" position="absolute" auto-scroll-to-top top="46px"></search>
 	<scroller lock-x height="-100" use-pullup use-pulldown  @on-pulldown-loading="refresh" @on-scroll="" @on-pullup-loading="loadMore" ref="scroller">
       	<div class="contant">
 			<cell v-for="item in filteredItems" :title="item.title" :link="{path:'/node_detail',query: { id: item.id }}" :inline-desc='item.header?item.header:"暂无简介"'></cell>
 		</div>
     </scroller>
-	
+    <alert v-model="alert" title="温馨提示" button-text="确认">{{text}}</alert>
 	 <div @click.stop="$refs.scroller.reset({top:0})" class="backTop">^</div>
 		 <div @click.stop="search" class="searchNode backTop"><icon type="search"></icon></div>
 	 <x-footer></x-footer>
@@ -19,7 +19,7 @@
 import XHeader from 'components/header'
 import XFooter from 'components/footer'
 import XLoading from "components/loading";
-import { Cell ,Search ,Scroller,Icon} from 'vux'
+import { Cell ,Search ,Scroller,Icon,Alert } from 'vux'
 export default {
   name: 'node',
   components:{
@@ -29,7 +29,8 @@ export default {
   	Search,
   	XLoading,
   	Scroller,
-  	Icon
+  	Icon,
+    Alert 
   },
   data () {
     return {
@@ -39,7 +40,11 @@ export default {
     	loading:false,
     	searchShow:false,
     	word:'',
-      page:0
+      page:10,
+      alert:false,
+      text:'',
+      results:[],
+      searchData:[]
     }
   },
    computed:{
@@ -53,7 +58,15 @@ export default {
   	 this.$nextTick(() => {
       this.$refs.scroller.reset({top: 0})
     })
-    this.$data.newList=this.$data.list.slice(0,10)
+     let that = this
+     let obj = {}
+     for(let i=0;i<that.$data.list.length;i++){
+        obj.name = that.$data.list[i].title
+        obj.id = that.$data.list[i].id
+        that.$data.searchData.push(obj)
+        obj={}
+     }
+     console.log(this.$data.searchData)
   },
 
   methods:{
@@ -73,10 +86,14 @@ export default {
                     that.$nextTick(function(){
                          that.$data.loading = false;
                       });
+      that.$data.newList=that.$data.newList.concat(this.$data.list.slice(0, 10))
+
             })
           .catch(function(err){
-             console.log("请求失败，原因如下："+err);
-               that.$data.loading = false;
+             console.log("请求失败，原因如下："+err)
+             that.$data.text='请求失败，原因如下'+err
+             that.$data.alert=true
+             that.$data.loading = false
           })
   
           }else{
@@ -84,23 +101,38 @@ export default {
             that.$data.list = JSON.parse(storage.getItem(key))
            that.$nextTick(function(){
                          that.$data.loading = false;
-                      });
-          
+          });
+          that.$data.newList=that.$data.newList.concat(this.$data.list.slice(0, 10))
         }
   	},
   	search:function(){
   		this.$data.searchShow = !this.$data.searchShow;
   	},
-  	getResult:function(){
-
+  	getResult (val){
+        console.log(val)
+        this.results = val ? this.searchNodes(val) : []
   	},
+    clickResuilt:function(item){
+        //window.alert('you click the result item: ' + JSON.stringify(item))
+        this.$router.push({ path: '/node_detail', query: { id: item.id }})
+    },
   	loadMore:function(){
-      this.$data.newList=this.$data.newList.concat(this.$data.list.slice(this.$data.page, this.$data.page+10))
-  		this.$data.page+=10
-      this.$nextTick(() => {
+      this.$data.newList=this.$data.newList.concat(this.$data.list.slice(this.$data.page, this.$data.page+10))     
+      if(this.$data.page<=this.$data.list.length){
+         this.$data.page+=10
+         this.$nextTick(() => {
         this.$refs.scroller.reset()
       })
       this.$refs.scroller.donePullup()
+      }else{ 
+        console.log("over")
+        this.$data.text='已经全部加载了哦'
+        this.$data.alert=true
+        this.$refs.scroller.disablePullup()
+        return false
+      }
+  		 console.log(this.$data.page)
+      
   	},
     refresh () {
       let that = this;
@@ -114,16 +146,48 @@ export default {
                     that.$nextTick(() => {
                       that.$refs.scroller.reset()
                     })
+                    that.$data.text='更新成功！'
+                    that.$data.alert=true
                     that.$refs.scroller.donePulldown()
             })
           .catch(function(err){
-             console.log("请求失败，原因如下："+err);
-              
+             console.log("更新失败，原因如下："+err)
+             that.$data.text='更新失败，原因如下'+err
+             that.$data.alert=true
+             that.$refs.scroller.donePulldown()
           })
+    },
+    searchNodes:function(val){
+      let rs = []
+      let name=''
+      let searchResults = this.filter(val)
+      if(searchResults.length>0){
+         for(let i=0;i<searchResults.length;i++){
+           rs.push({
+            title: searchResults[i].name,
+            id:searchResults[i].id
+          })
+        }
+      }else{
+        rs.push({
+            title:`您输入的 ${val} 暂无此主题`
+          })
+      }
+     
+       
+      return rs
+    },
+    filter:function(val){
+
+        return this.$data.searchData.filter(function(el) {
+          console.log(el.name)
+         return el.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      })
     }
   }
 
-};
+}
+
 </script>
 
 <style lang="less">
@@ -154,4 +218,14 @@ export default {
 	.searchNode{
 		bottom:130px;
 	} 
+  .weui-cells.vux-search_show{
+    height: 420px;
+    overflow-y:scroll!important;
+    -webkit-overflow-scrolling: touch;
+    -webkit-transform: translate3d(0, 0, 0);
+    -moz-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    transition: all .5s;
+  }
 </style>
